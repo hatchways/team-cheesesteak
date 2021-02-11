18d84ea5b42592323f7b6ac708fb296227534c5a
Matthew Amstutz <37874207+SilverStrings024@users.noreply.github.com>
2021-02-10 21:13:43 -0500

    Update db.py
    Add some variables for use in a string to connect SQLAlchemy to a PostgreSQL Database.
    Changed 'engine' to use a formatted string that will be used to connect the SQLAlchemy engine to a local PostgreSQL database.

--- server/db.py
+++ server/db.py
@@ -7,7 +7,13 @@ and import it everywhere else
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-# To be replaced with the actual engine
-engine = create_engine("sqlite:///:memory:", echo=True)
+
+# Fill these fields with your local DB information
+username = ""
+password = ""
+port = ""
+db_name = ""
+db_string = f"postgresql+psycopg2://{username}:{password}}@localhost:{port}/{db_name}"
+engine = create_engine(db_string, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
