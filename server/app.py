1f3f8479b1f570d201936f56aeb726067fdedbf7
DerinAde <derin.ade15@gmail.com>
2021-02-12 12:41:29 -0700

    moved profile.py to models folder

--- server/app.py
+++ server/app.py
@@ -4,15 +4,8 @@ from api.ping_handler import ping_handler
 from api.home_handler import home_handler
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:cheesesteak@localhost:5432/team-cheesesteak'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-    
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
-import profile
