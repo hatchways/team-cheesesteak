8467f0032a616b1bf87e0e6f81bf263b0ca56c60
Pierre Smith <smith.pierre@me.com>
2021-02-16 16:30:56 -0500

    TopBar changes (Provider added)

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,34 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
+import UserContext from "./context/User"
+
 import LandingPage from "./pages/Landing";
+import TopBar from "./components/TopBar";
 
 import "./App.css";
 
+
+
+
 function App() {
+  const user = {
+    FName: 'Pierre',
+    LName: 'Smith',
+    Province: 'Ontario',
+    PostalCode: 'M4C 2R2',
+    City: 'Toronto',
+    role: 'Chef',
+  }
+
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+      <UserContext.Provider value={user}>
+        <BrowserRouter>
+            <TopBar />
+          <Route path="/" component={LandingPage} />
+        </BrowserRouter>
+      </UserContext.Provider>
     </MuiThemeProvider>
   );
 }
