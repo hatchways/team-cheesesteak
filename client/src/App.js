24f45086c7ee901da123b3c92c17003f2294569c
DerinAde <derin.ade15@gmail.com>
2021-02-18 15:12:40 -0700

    changes for conflict

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+
 import ChefProfile from "./pages/ChefProfile";
 
 import "./App.css";
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -17,11 +17,4 @@ export const theme = createMuiTheme({
     primary: { main: "#ff743d" },
     secondary: { main: "#F8F8FF" },
   },
-  primary: "#f04040",
-  secondary: "#1f1f1f",
-  error: "#d8000c",
-  main: "#FF743D",
-  light: "#FF510C",
-  // Background color from the "fonts+colors.docx" google drive doc.
-  bgcolor: "#F8F8FF",
 });
