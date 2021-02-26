b1bd1c1e1c8fd2924a89a8d3896c08fc4c29f718
Matthew Amstutz <37874207+SilverStrings024@users.noreply.github.com>
2021-02-26 15:38:33 -0500

    Modify views.py and add new method to Recipe modell
    Added new method to the Recipe model to remove the need for repeated code and replace it with a loop
    
    Updated views.py  to import app and other things.

--- server/api/views.py
+++ server/api/views.py
@@ -1,87 +1,140 @@
 from flask import (    
-    request, make_response, 
+    request, jsonify
 )
 from models.recipe import Recipe
 from models.user import User
 from models.profile import Profile
+from app import app
+from auth import authenticate
 
-
-@app.route("/recipe/add/", methods=["POST"])
-def add_recipe(chef_id):
-    request_dict = request.form.to_dict()
+@app.route("/recipe/create/", methods=["POST"])
+@authenticate
+def create_recipe(**kwargs):
+    request_dict = request.get_json()
     recipe_info = {}
+    response_dict = {'recipe': {}}
     for field in Recipe.get_fields():
         if request_dict.get(field, None) != None:
             recipe_info[field] = request_dict.get(field)
     try:
         recipe = Recipe.create(**recipe_info)
-        response_dict = recipe.to_dict(excludes=['chef_profile_id', 'chef_profile', 'ingredients', 'required_items', 'image_urls'])
-        response_dict['price'] = recipe.get_formatted_price
-        response_dict['image_urls'] = recipe.get_image_url_list
-        response_dict['ingredients'] = recipe.get_ingredients_list
-        response_dict['required_items'] = recipe.get_utensil_list
-        
-        response = make_response(response_dict,201)
-        return response
+        kwargs['user'].profile.add_to_relationship('recipes', recipe)
+        response_dict['recipe'] = recipe.to_dict(excludes=[
+            'profile',
+            'profile_id',
+            'price',
+            'ingredients',
+            'required_items',
+            'image_urls'
+        ])
+        for field, value in recipe.get_formatted_info():
+            response_dict['recipe'][field] = value
+        response_dict['status'] = 200
+        response_dict['message'] = "Successfully created recipe"
+        return jsonify(response_dict), 201
     except AssertionError as e:
+        response_dict['status'] = 400
+        response_dict['message'] = "%s" % (e)
         # Validation problem
-        return {
-            'status': 400,
-            'message': "%s" % (e)
-        }
+        return jsonify(response_dict), 400
     
 
-@app.route("/recipe/<int:recipe_id>/edit", methods=["PUT"])
+@app.route("/recipe/edit", methods=["PUT"])
 def edit_recipe(recipe_id):    
-    request_dict = request.form.to_dict()
-    recipe_info = {}
+    request_dict = request.get_json()
+    recipe_id = request_dict.get('recipe_id')
+    recipe_info = {'recipe': {}}
+    response_dict = {}
     for field in Recipe.get_fields():
         if request_dict.get(field, None) != None:
             recipe_info[field] = request_dict.get(field)
     try:
         Recipe.update(recipe_id,**recipe_info)
-        return{
-        'status': 200,
-        'message': "Recipe updated successfully."
-    }
+        recipe = Recipe.get_instance(**{'id', recipe_id})
+        response_dict['recipe'] = recipe.to_dict(excludes=[
+            'profile',
+            'profile_id',
+            'price',
+            'ingredients',
+            'required_items',
+            'image_urls'
+        ])
+        for field, value in recipe.get_formatted_info():
+            response_dict['recipe'][field] = value
+        response_dict['status'] = 200
+        response_dict['message'] = "Successfully updated recipe"
+        return jsonify(response_dict), 200
     except AssertionError as e:
         # Validation problem
-        return {
-            'status': 400,
-            'message': "%s" % (e)
-        }
+        response_dict['status'] = 400
+        response_dict['message'] = "%s" % (e)
+        return jsonfiy(response_dict), 400
 
-@app.route("/recipe/<int:recipe_id>/delete", methods=["DELETE"])
-def delete_recipe(recipe_id):
+@app.route("/recipe/delete", methods=["DELETE"])
+@authenticate
+def delete_recipe(**kwargs):
+    response_dict = {}
+    recipe_id = request.get_json().get('recipe_id')
     try:
+        recipe = Recipe.get_instance(**{'id': recipe_id})
+        kwargs['user'].profile.remove_from_relationship('recipes', recipe)
         Recipe.delete(recipe_id)
-        return{
-            'status': 200,
-            'message': "Recipe deleted successfully."
-        }
-    except AssertionError as e:
-            # Validation problem
-            return {
-                'status': 400,
-                'message': "%s" % (e)
-            }
+        response_dict['status'] = 200
+        response_dict['message'] = "Recipe successfully deleted"
+        return jsonify(response_dict), 200
+   except AssertionError as e:
+       response_dict['status'] = 400
+       response_dict['message'] = "%s" % (e)
+       return jsonify(response_dict), 400
 
-@app.route("/user/<int:user_id>/edit",methods=["PUT"])
-def edit_user(user_id):    
+
+# The following 2 endpoints could possibly be
+# merged into one. This depends on the front
+# end design when it comes to editing information
+@app.route("/user/edit",methods=["PUT"])
+@authenticate
+def edit_user():
+    """
+    I'm not sure if I need to return the updated
+    information or not. The front end can use the
+    given values to udpate the display as long 
+    as the status is 200. If the front end will
+    re-render, this is not a concern. Otherwise 
+    this needs to return a new user information dictionary
+    """
     request_dict = request.form.to_dict()
     user_info = {}
     for field in User.get_fields():
         if request_dict.get(field, None) != None:
             user_info[field] = request_dict.get(field)
     try:
-        User.update(user_id,**user_info)
-        return{
-        'status': 200,
-        'message': "User details updated successfully."
-    }
+        User.update(user.id,**user_info)
+        # An updated user information dictionary will
+        # be returned on next re-render
+        response_dict['status'] = 200
+        response_dict['message'] = "Successfully updated information"
+        return jsonify(response_dict), 200
     except AssertionError as e:
         # Validation problem
-        return {
-            'status': 400,
-            'message': "%s" % (e)
-        }
+        response_dict['status'] = 400
+        response_dict['message'] = "%s" % (e)
+        return jsonify(response_dict), 400
+
+@app.route('/profile/edit', methods=["PUT"])
+@authenticate
+def edit_profile(**kwargs):
+    response_dict = {}
+    request_dict = request.get_json()
+    new_info = {}
+    for field in Profile.get_fields():
+        if request_dict.get(field, None) != None:
+            new_info[field] = request_dict.get(field)
+    try:
+        Profile.update(user.profile.id, **new_info)
+        response_dict['status'] = 200
+        response_dict['message'] = "Successfully updated profile"
+        return jsonify(response_dict), 200
+    except AssertionError as e:
+        response_dict['status'] = 400
+        response_dict['message'] = "%s" % (e)
+        return jsonify(response_dict), 400
